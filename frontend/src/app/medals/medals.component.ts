9311a4f1fe7a1d93e0d01e8b5446ba28c494f3ff
User <user@user.com>
2021-09-05 15:36:41 +0200

    Improved graphs

--- frontend/src/app/medals/medals.component.ts
+++ frontend/src/app/medals/medals.component.ts
@@ -17,14 +17,45 @@ export class MedalsComponent implements OnInit {
       this.allCountries = data;
     });
 
-    let chart = c3.generate({
-      bindto: '#chart',
+    c3.generate({
+      bindto: '#country-chart',
       
       data: {
         columns: [
-          ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25]
-        ]
+          ['Srbija', 10],
+          ['Grcka', 8],
+          ['SAD', 42],
+          ['Nemacka', 1],
+          ['Francuska', 8],
+          ['Rusija', 60]
+        ],
+        type: 'bar'
+      },
+      bar: {
+        width: {
+          ratio: 0.5
+        }
+      }
+    });
+
+    c3.generate({
+      bindto: '#sport-chart',
+      
+      data: {
+        columns: [
+          ['Srbija', 10],
+          ['Grcka', 8],
+          ['SAD', 42],
+          ['Nemacka', 1],
+          ['Francuska', 8],
+          ['Rusija', 60]
+        ],
+        type: 'bar'
+      },
+      bar: {
+        width: {
+          ratio: 0.5
+        }
       }
     });
   }
