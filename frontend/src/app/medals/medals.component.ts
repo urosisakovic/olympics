3f4a8a48ab17ad92d1b47dd7cdb01ae11b570b03
User <user@user.com>
2021-09-05 14:42:53 +0200

    Added c3 and a placeholder chart to the app

--- frontend/src/app/medals/medals.component.ts
+++ frontend/src/app/medals/medals.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../countries.service';
 import { Country } from '../data/country';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-medals',
@@ -14,6 +15,17 @@ export class MedalsComponent implements OnInit {
   ngOnInit(): void {
     this.countriesService.getAllCountries().subscribe((data: any) => {
       this.allCountries = data;
+    });
+
+    let chart = c3.generate({
+      bindto: '#chart',
+      
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      }
     });
   }
 
